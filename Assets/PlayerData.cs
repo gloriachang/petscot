2ee4150cac7d4ec2865377bc1e23a4e927707b97
gloriachang <changlo@umich.edu>
2026-09-07 11:41:57 -0400

    Assets, Packages, ProjectSettings

---
+++ Assets/PlayerData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public static string petName = "Friend";
+    public static string major = "Undeclared";
+    public static string affirmations = "";
+}
